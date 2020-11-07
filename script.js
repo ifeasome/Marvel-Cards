22e10bf1c4d547f858b16d91b040e25df9063b5a
vfavorito <vince.favorito@gmail.com>
2020-11-07 09:12:28 -0600

    Changed how searching heroes alphabetically functions

--- script.js
+++ script.js
@@ -7,13 +7,6 @@ $(document).ready(function () {
     $("#searchBar").append(searchBarBtn);
     $("#searchBarBtn").on("click", heroSearch);
 
-    // this is creating the search for heroes button that will call the function getHeroes when clicked
-    let alphaSearchButton = $("<button>");
-    alphaSearchButton.attr("id", "searchBtn");
-    alphaSearchButton.text("Search for Heroes Alphabetically");
-    $("#searchButton").append(alphaSearchButton);
-    $("#searchBtn").on("click", getHeroesAlphabet);
-
     // this function is taking whatever was entered into search bar and grapping the hero that matches that name and displaying it
     function heroSearch() {
         let heroNameSearch = $("#heroNameHere").val();
@@ -32,35 +25,53 @@ $(document).ready(function () {
             $("#heroPics").prepend(heroPicImg);
         })
     }
-    
+    // this is creating the search for heroes button that will call the function getHeroes when clicked
+    let alphaSearchButton = $("<button>");
+    alphaSearchButton.attr("id", "searchBtn");
+    alphaSearchButton.text("Search for Heroes Alphabetically");
+    $("#searchButton").append(alphaSearchButton);
+    $("#searchBtn").on("click", getHeroesAlphabet);
+
     // this function is running an ajax call in a for loop to display heroes alphabetically
     function getHeroesAlphabet() {
-        let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        $("#letterButtons").empty()
+        let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
         for (let i = 0; i < alphabet.length; i++) {
-            let queryURL = "https://gateway.marvel.com/v1/public/characters?nameStartsWith=" + alphabet[i] + "&limit=50&ts=1&apikey=041b36ff0606f85c2d365e1174d26db6&hash=88a95a7cb326797147690494db18ecdb";
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
-                console.log(response)
-                let alphaLabel = $("<h2>")
-                alphaLabel.text("Heroes Whose Name Starts With " + alphabet[i].toUpperCase() + " :");
-                $("#heroPics").append(alphaLabel);
-                for (let i = 0; i < 50; i++) {
-                    let heroExtension = response.data.results[i].thumbnail.extension
-                    let heroPic = response.data.results[i].thumbnail.path + "/portrait_xlarge." + heroExtension;
-                    let heroName = response.data.results[i].name
-                    if (heroPic.search("image_not_available") === -1 && heroName != "" && heroExtension === "jpg") {
-                        let heroPicImg = $("<img>");
-                        heroPicImg.addClass("heroPics");
-                        heroPicImg.attr("src", heroPic);
-                        heroPicImg.attr("data-name", heroName);
-                        $("#heroPics").append(heroPicImg);
-                    }
-                }
-            })
+            let letterButton = $("<button>");
+            letterButton.text(alphabet[i]);
+            letterButton.addClass("letterBtn");
+            letterButton.attr("data-letter", alphabet[i]);
+            $("#letterButtons").append(letterButton);
         }
     }
+
+    // this event listener triggers an ajax call that will search for heroes that start with the letter of the button clicked
+    $(document).on("click", ".letterBtn", function (event) {
+        $("#heroPics").empty();
+        let heroLetter = $(this).attr("data-letter");
+        let queryURL = "https://gateway.marvel.com/v1/public/characters?nameStartsWith=" + heroLetter + "&limit=50&ts=1&apikey=041b36ff0606f85c2d365e1174d26db6&hash=88a95a7cb326797147690494db18ecdb";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            let alphaLabel = $("<h2>")
+            alphaLabel.text("Heroes Whose Name Starts With " + event.currentTarget.attributes[1].value + " :");
+            $("#heroPics").append(alphaLabel);
+            for (let i = 0; i < 50; i++) {
+                let heroExtension = response.data.results[i].thumbnail.extension
+                let heroPic = response.data.results[i].thumbnail.path + "/portrait_xlarge." + heroExtension;
+                let heroName = response.data.results[i].name
+                if (heroPic.search("image_not_available") === -1 && heroName != "" && heroExtension === "jpg") {
+                    let heroPicImg = $("<img>");
+                    heroPicImg.addClass("heroPics");
+                    heroPicImg.attr("src", heroPic);
+                    heroPicImg.attr("data-name", heroName);
+                    $("#heroPics").append(heroPicImg);
+                }
+            }
+        })
+    });
     // this event listener will alert you the name of any hero clicked
     $(document).on("click", ".heroPics", function () {
         let heroName = $(this).attr("data-name");
