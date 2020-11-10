a0cadef7ca9376e56d81c94b68690f19a4d23188
buildgal <gjaquelin7@outlook.com>
2020-11-09 21:21:14 -0600

    adding the music player based off of the hero name

--- script.js
+++ script.js
@@ -177,6 +177,22 @@ $(document).ready(function () {
     $(document).on("click", ".heroPics", function () {
         let heroName = $(this).attr("data-name");
         alert(heroName);
+
+            //this API Calls Deezer using the heroName to generate a song associated with the character selected
+            let searchTerm=heroName;
+            let musicURL="https://cors-anywhere.herokuapp.com/"+"https://api.deezer.com/search/track?q="+searchTerm
+            $.ajax({
+            url: musicURL,
+            method: "GET"
+            }).then(function(response) {
+            let song=response.data[0].id;
+            let playerURL="https://www.deezer.com/plugins/player?format=square&autoplay=true&playlist=true&width=300&height=300&color=EF5466&layout=&size=medium&type=tracks&id="+
+            song+'&app_id=444442';
+            let deezerPlayer=$("#player");
+            deezerPlayer.attr("src", playerURL);
+    
     });
+    });
+    
 });
 
